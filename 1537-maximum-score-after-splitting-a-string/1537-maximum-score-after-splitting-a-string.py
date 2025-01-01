c1e8ea2d9fa4aede59b4d465050edd9b10e19ee4
Nistha <111463339+Nistha27@users.noreply.github.com>
2025-01-01 08:16:33 +0530

    Time: 3 ms (65.91%), Space: 12.5 MB (15.34%) - LeetHub

--- 1537-maximum-score-after-splitting-a-string/1537-maximum-score-after-splitting-a-string.py
+++ 1537-maximum-score-after-splitting-a-string/1537-maximum-score-after-splitting-a-string.py
@@ -4,13 +4,13 @@ class Solution(object):
         :type s: str
         :rtype: int
         """
-        zero=0
+        zero=0 # no of zeroes in the left substring
         ones=s.count("1")
         result=0
-        for i in range(len(s)-1):
+        for i in range(len(s)-1): # -1 because there should be non empty right substring
             if s[i]=="0":
-                zero+=1
+                zero+=1 
             else:
-                ones-=1
+                ones-=1 # decreasing the no of 1's as it is encountered in right substring
             result=max(result,zero+ones)
         return result
