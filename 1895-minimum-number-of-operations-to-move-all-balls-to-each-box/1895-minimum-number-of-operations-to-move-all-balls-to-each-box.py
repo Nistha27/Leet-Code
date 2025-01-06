1cebaa87291b8d613e30570a1aa125dd3ed82f09
Nistha <111463339+Nistha27@users.noreply.github.com>
2025-01-06 23:08:05 +0530

    Time: 22 ms (68.67%), Space: 12.9 MB (16.87%) - LeetHub

--- 1895-minimum-number-of-operations-to-move-all-balls-to-each-box/1895-minimum-number-of-operations-to-move-all-balls-to-each-box.py
+++ 1895-minimum-number-of-operations-to-move-all-balls-to-each-box/1895-minimum-number-of-operations-to-move-all-balls-to-each-box.py
@@ -4,9 +4,26 @@ class Solution(object):
         :type boxes: str
         :rtype: List[int]
         """
-        res=[0]*len(boxes)
+        #Brute Force
+        '''res=[0]*len(boxes)
         for i in range(len(boxes)):
             for j in range(len(boxes)):
                 if boxes[j]=="1":
                     res[i]+=abs(j-i)
+        return res'''
+
+        #Optimal Solution
+        res=[0]*len(boxes)
+
+        balls,moves=0,0
+        for i in range(len(boxes)):
+            res[i]=balls+moves
+            moves=moves+balls
+            balls+=int(boxes[i])
+
+        balls,moves=0,0
+        for i in reversed(range(len(boxes))):
+            res[i]+=balls+moves
+            moves=moves+balls
+            balls+=int(boxes[i])
         return res
