3d628b25b6a74a8ac59984a216d6a5a4ec376486
Nistha <111463339+Nistha27@users.noreply.github.com>
2025-01-17 19:10:07 +0530

    Time: 51 ms (80%), Space: 15.3 MB (80%) - LeetHub

---
+++ 2792-neighboring-bitwise-xor/2792-neighboring-bitwise-xor.py
@@ -0,0 +1,12 @@
+class Solution(object):
+    def doesValidArrayExist(self, derived):
+        """
+        :type derived: List[int]
+        :rtype: bool
+        """
+        first=0
+        last=0
+        for n in derived:
+            if n:
+                last=~last
+        return first==last
