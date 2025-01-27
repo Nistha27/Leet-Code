069f7c6b337debe2280b847ac4b232d50ae6ad66
Nistha <111463339+Nistha27@users.noreply.github.com>
2025-01-27 10:00:35 +0530

    Time: 30 ms (94.12%), Space: 15.5 MB (15.69%) - LeetHub

---
+++ 1558-course-schedule-iv/1558-course-schedule-iv.py
@@ -0,0 +1,28 @@
+class Solution(object):
+    def checkIfPrerequisite(self, numCourses, prerequisites, queries):
+        """
+        :type numCourses: int
+        :type prerequisites: List[List[int]]
+        :type queries: List[List[int]]
+        :rtype: List[bool]
+        """
+        adj=defaultdict(list)
+        for prereq,crs in prerequisites:
+            adj[crs].append(prereq)
+        
+        def dfs(crs):
+            if crs  not in prereqMap:
+                prereqMap[crs]=set()
+                for prereq in  adj[crs]:
+                    prereqMap[crs] |= dfs(prereq)
+                prereqMap[crs].add(crs)
+            return prereqMap[crs]
+
+        prereqMap={}
+        for crs in range(numCourses):
+            dfs(crs)
+        res=[]
+        for u,v in queries:
+            res.append(u in prereqMap[v])
+        return res
+        
