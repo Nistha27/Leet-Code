937c9bf435613bac4449924f1f3e062e6a4b0526
Nistha <111463339+Nistha27@users.noreply.github.com>
2025-02-06 18:57:55 +0530

    Time: 859 ms (22.22%), Space: 67.7 MB (22.22%) - LeetHub

---
+++ 1364-tuple-with-same-product/1364-tuple-with-same-product.py
@@ -0,0 +1,18 @@
+class Solution(object):
+    def tupleSameProduct(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        product_cnt=defaultdict(int)
+        pair_cnt=defaultdict(int)
+
+        for i in range(len(nums)):
+            for j in range(i+1,len(nums)):
+                product = nums[i]*nums[j]
+                pair_cnt[product]+=product_cnt[product] 
+                product_cnt[product]+=1
+        res=0
+        for cnt in pair_cnt.values():
+            res+=8*cnt
+        return res
