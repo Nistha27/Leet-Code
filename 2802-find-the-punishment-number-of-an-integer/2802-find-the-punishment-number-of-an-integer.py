51c115333a106416001abe5cefbe8ce4f11b8788
Nistha <111463339+Nistha27@users.noreply.github.com>
2025-02-15 22:22:51 +0530

    Time: 2599 ms (0%), Space: 12.2 MB (100%) - LeetHub

---
+++ 2802-find-the-punishment-number-of-an-integer/2802-find-the-punishment-number-of-an-integer.py
@@ -0,0 +1,20 @@
+class Solution(object):
+    def punishmentNumber(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        def partition (i,cur,target,string):
+            if i==len(string) and cur==target:
+                return True
+            for j in range(i,len(string)):
+                string[i:j+1]
+                if partition(j+1,cur+int(string[i:j+1]),target,string):
+                    return True
+            return False
+
+        res=0
+        for i in range(1,n+1):
+            if partition(0,0,i,str(i*i)):
+                res+=i*i
+        return res
