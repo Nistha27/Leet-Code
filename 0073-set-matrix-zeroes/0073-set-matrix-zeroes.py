c780230fb08c4f1d4835e636b148ece811e0d6a1
Nistha <111463339+Nistha27@users.noreply.github.com>
2025-03-16 00:07:51 +0530

    Time: 7 ms (47.86%), Space: 13.1 MB (43.2%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.py
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.py
@@ -1,12 +1,9 @@
 class Solution(object):
     def setZeroes(self, matrix):
-        """
-        :type matrix: List[List[int]]
-        :rtype: None Do not return anything, modify matrix in-place instead.
-        """
-        m=len(matrix)
-        n=len(matrix[0])
-        def markcol(matrix,i,n):
+        #Brute Force
+        n=len(matrix)
+        m=len(matrix[0])
+        '''def markcol(matrix,i,n):
             for j in range(n):
                 if matrix[i][j]!=0:
                     matrix[i][j]="#"
@@ -24,4 +21,25 @@ class Solution(object):
         for i in range(m):
             for j in range(n):
                 if matrix[i][j]=="#":
-                    matrix[i][j]=0       
+                    matrix[i][j]=0'''
+
+        #Better Approach
+
+        row=[0]*n
+        col=[0]*m
+
+        for i in range(n):
+            for j in range(m):
+                if matrix[i][j]==0:
+                    row[i]=1
+                    col[j]=1
+
+        for i in range(n):
+            for j in range(m):
+                if row[i] or col[j]:
+                    matrix[i][j]=0
+    
+
+
+
+    
