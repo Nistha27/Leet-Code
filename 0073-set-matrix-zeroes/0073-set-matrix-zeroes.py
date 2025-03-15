dad1a2eb3f5580ab2bd443a08da7749446c52578
Nistha <111463339+Nistha27@users.noreply.github.com>
2025-03-16 00:09:22 +0530

    Time: 7 ms (47.86%), Space: 13.1 MB (74.18%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.py
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.py
@@ -1,8 +1,8 @@
 class Solution(object):
     def setZeroes(self, matrix):
         #Brute Force
-        n=len(matrix)
-        m=len(matrix[0])
+        #n=len(matrix)
+        #m=len(matrix[0])
         '''def markcol(matrix,i,n):
             for j in range(n):
                 if matrix[i][j]!=0:
@@ -25,7 +25,7 @@ class Solution(object):
 
         #Better Approach
 
-        row=[0]*n
+        '''row=[0]*n
         col=[0]*m
 
         for i in range(n):
@@ -37,7 +37,26 @@ class Solution(object):
         for i in range(n):
             for j in range(m):
                 if row[i] or col[j]:
-                    matrix[i][j]=0
+                    matrix[i][j]=0'''
+
+        #Optimal Approach
+        rows = set()
+        cols = set()
+        n, m = len(matrix), len(matrix[0])
+        # Find first rows and cols
+        for i in range(n):
+            for j in range(m):
+                if matrix[i][j] == 0:
+                    rows.add(i)
+                    cols.add(j)
+        # Set rows to zero
+        for i in rows:
+            for j in range(m):
+                matrix[i][j] = 0
+        # Set cols to zero
+        for i in range(n):
+            for j in cols:
+                matrix[i][j] = 0
     
 
 
