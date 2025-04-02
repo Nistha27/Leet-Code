5ae747103f643e526cdf282b8596211684629d2e
Nistha <111463339+Nistha27@users.noreply.github.com>
2025-04-02 23:23:27 +0530

    Time: 90 ms (52.5%), Space: 48.7 MB (42.5%) - LeetHub

--- 2262-solving-questions-with-brainpower/2262-solving-questions-with-brainpower.py
+++ 2262-solving-questions-with-brainpower/2262-solving-questions-with-brainpower.py
@@ -1,20 +1,20 @@
 class Solution(object):
     def mostPoints(self, questions):
-        """
-        :type questions: List[List[int]]
-        :rtype: int
-        """
-        cache=[0]*len(questions)
+        N=len(questions)
+        cache=[0]*N
+        for i in reversed(range(N)):
+            points,brainpower=questions[i]
+            next_i=i+1+brainpower 
+
+            choose=points+ (cache[next_i ] if next_i < N else 0)
+            skip=cache[i+1] if i+1 < N else 0
+            cache[i]=max(choose,skip)
+        return cache[0]
+
         def backtrack(i):
             if i>=len(questions):
                 return 0
             if cache[i]:
                 return cache[i]
-
-            points,brainpower=questions[i]
-            cache[i]= max(backtrack(i+1),#skip
-                    points+backtrack(i+1+brainpower)#choose
-                )
-            return cache[i]
-        return backtrack(0)
+        
         
