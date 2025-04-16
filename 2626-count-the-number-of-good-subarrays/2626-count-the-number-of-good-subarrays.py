02bcaa28cc5cb69ae606fb91c99ac05f087522a9
Nistha <111463339+Nistha27@users.noreply.github.com>
2025-04-16 23:07:51 +0530

    Time: 186 ms (16.67%), Space: 29 MB (83.33%) - LeetHub

---
+++ 2626-count-the-number-of-good-subarrays/2626-count-the-number-of-good-subarrays.py
@@ -0,0 +1,26 @@
+class Solution:
+    def countGood(self, nums, k): 
+        n = len(nums)
+        left = 0
+        right = 0
+        good_subarrays = 0
+        freq = defaultdict(int)
+        equal_pairs = 0
+
+        while left < n:
+            while right < n and equal_pairs < k:
+                freq[nums[right]] += 1
+                if freq[nums[right]] >= 2:
+                    equal_pairs += freq[nums[right]] - 1
+                right += 1
+            
+            if equal_pairs >= k:
+                good_subarrays += n - right + 1
+
+            # Remove left item
+            freq[nums[left]] -= 1
+            if freq[nums[left]] > 0:
+                equal_pairs -= freq[nums[left]]
+            left += 1
+
+        return good_subarrays
