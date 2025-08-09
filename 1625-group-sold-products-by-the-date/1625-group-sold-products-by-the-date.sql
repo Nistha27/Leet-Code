093b9af8736869a0e4d2d906be4e286f92bb7a23
Nistha <111463339+Nistha27@users.noreply.github.com>
2025-08-10 00:49:17 +0530

    Time: 442 ms (59.81%), Space: 0B (100%) - LeetHub

---
+++ 1625-group-sold-products-by-the-date/1625-group-sold-products-by-the-date.sql
@@ -0,0 +1,6 @@
+# Write your MySQL query statement below
+SELECT sell_date , COUNT(distinct product) as num_sold, 
+group_concat(distinct product) as products
+FROM Activities
+GROUP BY sell_date
+ORDER BY sell_date
