f4b372ea18388424613a8429fd7937f9cb8e18f8
Nistha <111463339+Nistha27@users.noreply.github.com>
2025-08-13 11:40:10 +0530

    Time: 280 ms (42.88%), Space: 0B (100%) - LeetHub

--- 0176-second-highest-salary/0176-second-highest-salary.sql
+++ 0176-second-highest-salary/0176-second-highest-salary.sql
@@ -1,14 +1,8 @@
-WITH cte AS (
-    SELECT 
-        salary,
-        DENSE_RANK() OVER (ORDER BY salary DESC) AS rank_sal
-    FROM Employee
-)
-
 SELECT 
-    CASE 
-        WHEN COUNT(*) = 0 THEN NULL
-        ELSE MAX(salary)
-    END AS SecondHighestSalary
-FROM cte
-WHERE rank_sal = 2;
+CASE 
+WHEN Count(*)=0 THEN NULL
+ELSE max(salary) 
+END AS SecondHighestSalary
+FROM Employee
+WHERE salary<(SELECT MAX(salary) from Employee)
+
