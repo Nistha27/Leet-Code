68d1fdc9b89c78451b137f94de7095bc003c2cbc
Nistha <111463339+Nistha27@users.noreply.github.com>
2025-09-25 21:55:21 +0530

    Time: 0 ms (100%), Space: 12.5 MB (52.27%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.py
+++ 0075-sort-colors/0075-sort-colors.py
@@ -4,12 +4,5 @@ class Solution(object):
         :type nums: List[int]
         :rtype: None Do not return anything, modify nums in-place instead.
         """
-        for i in range(len(nums)):
-            key=nums[i]
-            j=i-1
-            while j>=0 and nums[j]>key:
-                nums[j+1]=nums[j]
-                j-=1
-            nums[j+1]=key
-        
+        nums.sort()
         
